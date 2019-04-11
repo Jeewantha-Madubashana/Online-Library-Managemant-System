74972d5ce7a9b86e388b5b52d50733bc78a00cef
Jeewantha95 <jeewaplayacc@gmail.com>
2019-04-11 13:41:43 +0530

    initial Commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; B/Katugalla Widyalaya | Online Library Management System | Designed by : N.G.I.Madushani</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
